821e36696f3247ff9b2eee871bb6b4299377a243
Zack <lorenx@163.com>
2022-02-12 16:32:26 +0800

    Update logger

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -1,26 +1,39 @@
 import {
   Injectable,
   Scope,
-  Logger as NestLogger,
   LoggerService as NestLoggerService,
   LogLevel,
   Optional,
 } from '@nestjs/common';
-import type { Logger } from 'winston';
-import DailyRotateFile = require('winston-daily-rotate-file');
-import { createLogger, format, transports } from 'winston';
+import { Logger } from 'winston';
 
-const {
-  combine,
-  timestamp,
-  label,
-  prettyPrint,
-  metadata,
-  ms,
-  json,
-  colorize,
-  splat,
-} = format;
+import DailyRotateFile = require('winston-daily-rotate-file');
+import {
+  createLogger,
+  format,
+  level as WinstonLogLevel,
+  transports,
+} from 'winston';
+export const isString = (val: any): val is string => typeof val === 'string';
+
+const { combine, timestamp, colorize, splat, prettyPrint, ms, printf } = format;
+
+const loggerFormat = [
+  timestamp({ format: 'YYYY-MM-DD A HH:mm:ss.SSS' }),
+  prettyPrint(),
+  ms(),
+  printf((info) => {
+    // if (info.stack) {
+    //   info.message = info.stack;
+    // }
+    console.log(info)
+    const stack = info.stack ? `\n${info.stack}` : ''
+    return `[${process.pid}]${info.timestamp} ${info.level} [${
+      info.context || 'Application'
+    }] ${info.ms} ${info.message}${stack}`;
+    // [Nest] 58402  - 2022/02/11 下午3:40:43     LOG [NestFactory] Starting Nest application...
+  }),
+];
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class LoggerService implements NestLoggerService {
@@ -39,39 +52,26 @@ export class LoggerService implements NestLoggerService {
       const name = 'aorajs';
       LoggerService.staticInstanceRef = createLogger({
         exitOnError: false,
-        format: combine(
-          colorize({
-            colors: {
-              error: 'red',
-              info: 'green',
-              warn: 'yellow',
-              debug: 'green',
-              verbose: 'red',
-            },
-          }),
-          splat(),
-          timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-          format.printf((info) => {
-            if (info.stack) {
-              info.message = info.stack;
-            }
-            return `${info.timestamp} [${process.pid}] ${info.level}: [${
-              info.context || 'Application'
-            }] ${info.message}`;
-          }),
-        ),
         defaultMeta: { pid: process.pid },
         transports: [
-          new transports.Console(),
+          new transports.Console({
+            format: combine(
+              colorize({
+                all: true,
+              }),
+              ...loggerFormat,
+            ),
+          }),
           new DailyRotateFile({
             filename: `${name}-%DATE%.log`,
             dirname: 'storages/logs',
-            datePattern: 'YYYY-MM-DD-HH',
+            datePattern: 'YYYYMMDD-HH',
             zippedArchive: true,
             handleExceptions: true,
             maxSize: '20m',
             json: false,
             maxFiles: '14d',
+            format: combine(...loggerFormat),
           }),
         ],
       });
@@ -79,23 +79,101 @@ export class LoggerService implements NestLoggerService {
     return LoggerService.staticInstanceRef;
   }
 
+  log(message: any, context?: string): void;
+  log(message: any, ...optionalParams: [...any, string?]): void;
   log(message: any, ...optionalParams: any[]) {
-    this.loggerInstance.log('info', message, ...optionalParams);
+    const { messages, context } = this.getContextAndMessagesToPrint([
+      message,
+      ...optionalParams,
+    ]);
+    this.printMessages(messages, context, 'info');
   }
 
+  error(message: any, stack?: string, context?: string): void;
+  error(message: any, ...optionalParams: [...any, string?, string?]): void;
   error(message: any, ...optionalParams: any[]) {
-    this.loggerInstance.error(message, ...optionalParams);
+    const { messages, context, stack } =
+      this.getContextAndStackAndMessagesToPrint([message, ...optionalParams]);
+    this.printMessages(messages, context, 'error');
+    this.loggerInstance.log('error', message as string, { context, stack });
   }
+
+  warn(message: any, context?: string): void;
+  warn(message: any, ...optionalParams: [...any, string?]): void;
   warn(message: any, ...optionalParams: any[]) {
-    this.loggerInstance.warn(message, ...optionalParams);
+    const { messages, context } = this.getContextAndMessagesToPrint([
+      message,
+      ...optionalParams,
+    ]);
+    this.printMessages(messages, context, 'warn');
   }
+
+  debug(message: any, context?: string): void;
+  debug(message: any, ...optionalParams: [...any, string?]): void;
   debug(message: any, ...optionalParams: any[]) {
-    this.loggerInstance.debug(message, ...optionalParams);
+    const { messages, context } = this.getContextAndMessagesToPrint([
+      message,
+      ...optionalParams,
+    ]);
+    this.printMessages(messages, context, 'debug');
   }
+
+  verbose(message: any, context?: string): void;
+  verbose(message: any, ...optionalParams: [...any, string?]): void;
   verbose(message: any, ...optionalParams: any[]) {
-    this.loggerInstance.verbose(message, ...optionalParams);
+    const { messages, context } = this.getContextAndMessagesToPrint([
+      message,
+      ...optionalParams,
+    ]);
+    this.printMessages(messages, context, 'verbose');
   }
-  setLogLevels?(levels: LogLevel[]) {
-    //
+
+  setLogLevels?(levels: (LogLevel & WinstonLogLevel)[]) {
+    // this.loggerInstance.configure({
+    // levels: levels as WinstonLogLevel[]
+    // });
+  }
+
+  private getContextAndMessagesToPrint(args: unknown[]) {
+    if (args?.length <= 1) {
+      return { messages: args, context: this.context };
+    }
+    const lastElement = args[args.length - 1];
+    const isContext = isString(lastElement);
+    if (!isContext) {
+      return { messages: args, context: this.context };
+    }
+    return {
+      context: lastElement as string,
+      messages: args.slice(0, args.length - 1),
+    };
+  }
+
+  private getContextAndStackAndMessagesToPrint(args: unknown[]) {
+    const { messages, context } = this.getContextAndMessagesToPrint(args);
+    console.log(args)
+    if (messages?.length <= 1) {
+      return { messages, context };
+    }
+    const lastElement = messages[messages.length - 1];
+    const isStack = isString(lastElement);
+    if (isStack) {
+      return { messages, context };
+    }
+    return {
+      stack: lastElement as string,
+      messages: messages.slice(0, messages.length - 1),
+      context,
+    };
+  }
+
+  protected printMessages(
+    messages: unknown[],
+    context = '',
+    logLevel: WinstonLogLevel = 'info',
+  ) {
+    messages.forEach((message) => {
+      this.loggerInstance.log(logLevel, message as string, { context });
+    });
   }
 }
